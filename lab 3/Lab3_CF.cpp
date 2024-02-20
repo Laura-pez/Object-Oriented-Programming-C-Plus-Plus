7d9e2ed9ec8b5977df2a0490b29225ff2b6fd881
Laura-pez <157765528+Laura-pez@users.noreply.github.com>
2024-02-19 23:31:27 -0800

    Lab3_CF.cpp

--- lab 3/Lab3_CF.cpp
+++ lab 3/Lab3_CF.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+  int x=0;
+  int y=5;
+
+  while(x<y){
+    printf("%i\n",x);
+    x = x + 3;
+  }
+
+  if(x >= y){
+    if(x%2 ==0){
+      printf("x is even");
+    }
+
+    else{
+      printf("x is odd");
+    }
+  }
+}
