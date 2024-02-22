e723860a2cbafbb5b8df9ec604c9293635f1f697
Laura-pez <157765528+Laura-pez@users.noreply.github.com>
2024-02-22 15:58:37 -0800

    Update Lab4_func.cpp

--- lab4/Lab4_func.cpp
+++ lab4/Lab4_func.cpp
@@ -1,1 +1,60 @@
+#include <iostream>
+using namespace std;
 
+/** sumOfThree
+* @brief returns the sum of x, y, and z
+*
+* @param x : (int)
+* @param y : (int)
+* @param z : (int)
+* @return int
+*/
+int sumOfThree(int x, int y, int z){
+  return x + y + z;
+}
+
+/** division
+* @brief returns x / y
+*
+* @param x : (double)
+* @param y : (double)
+* @return double
+*/
+double division(double x, double y){
+  return x / y;
+}
+/** isGreatherThan
+* @brief returns whether x is greater than y
+*
+* @param x : (double)
+* @param y : (int)
+* @return true : x is greater than y
+* @return false : x is less than or equal to y
+*/
+bool isGreatherThan(double x, int y){
+  return x > y;
+}
+/** select
+* @brief output the statements based on func_type
+* func_type : output
+* "sum" : "The sum is "
+* "division" : "The quotient is "
+* "comparison" : "The first value being greater than the second value is "
+* All other phrases: "Invalid function type\n"
+* @param func_type : (string)
+* @return string
+*/
+string select(string func_type)
+if(func_type == "sum"){
+  out = "the sum is ";
+}
+else if(func_type == "division"){
+  out ="The quotient is ";
+}
+  return "The quotient is ";{
+else if(func_type == "comparison")
+out = "The first value being greater than the second value is ";
+}
+else out = "Invalid function type\n";{
+  return out;
+}
