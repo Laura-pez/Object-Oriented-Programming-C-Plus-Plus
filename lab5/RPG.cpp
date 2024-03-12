bc3b8f23e6a3e64aa3ca282a844db216b5247207
Laura-pez <157765528+Laura-pez@users.noreply.github.com>
2024-03-12 00:05:30 -0700

    Update RPG.cpp

--- lab5/RPG.cpp
+++ lab5/RPG.cpp
@@ -34,19 +34,37 @@ RPG::RPG(string name, int health, int strength, int defense, string type){
     this->strength = strength;
     this->defense = defense;
     this->type = type;
+    setSkills();
 
 
 }
 
 void RPG::setSkills(){
-    
-}
- 
-void RPG::printAction(string, RPG){
-}
+    if(type =="mage"){
+        skills[0] = "fire";
+        skills[1] =  "thunder";
+    }else if (type=="thief"){
+        skills[0] = "pilfer";
+        skills[1] =  "jab"; 
+    } else if (type=="archer"){
+        skills[0] = "parry";
+        skills[1] =  "crossbow_attack"; 
+    }else {
+        skills[0] = "slash";
+        skills[1] =  "parry"; 
+    }
 
+}
+void RPG::printAction(string skill, RPG opponent){
+    printf("%s used %s on %\n", name.c_str(), skill.c_str(), opponent.getName().c_str());
+}
+/**
+
+* @brief updates health into new_health
+*
+* @param new_health
+*/  
 void RPG::updateHealth(int){
-    
 }
   
 void RPG::attack(RPG *){
@@ -54,7 +72,12 @@ void RPG::attack(RPG *){
 
 void RPG::useSkill(RPG *){
 }
-
+/**
+* @brief returns whether health is greater than 0
+*
+* @return true
+* @return false
+*/
 bool RPG::isAlive() const{
     return false;
 }
@@ -64,13 +87,13 @@ string RPG::getName() const{
 }
 
 int RPG::getHealth() const{
-    return 0;
+    return health > 0;
 }
 
 int RPG::getStrength() const{
-    return 0;
+    return strength;
 }
 
 int RPG::getDefense() const{
-    return 0;
+    return defense;
 }
