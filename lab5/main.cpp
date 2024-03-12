dbbe2262db117bed115331c6e6eff11dda1c65aa
Laura-pez <157765528+Laura-pez@users.noreply.github.com>
2024-03-12 00:05:33 -0700

    Update main.cpp

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include "RPG.h"
 
+using namespace std;
+
+int main() {
+    RPG npc; 
+    RPG player("Player", 100, 10, 10, "warrior");
+    cout << "Player name: " << player.getName() << endl;
+    cout << "Player health: " << player.getHealth() << endl;
+
+    cout << "NPC name: " << npc.getName() << endl;
+    cout << "NPC health: " << npc.getHealth() << endl;
+    
+
+    npc.updateHealth(80);
+    player.updateHealth(100);
+
+    cout << "Updated NPC health: " << npc.getHealth() << endl;
+    cout << "Updated player health: " << player.getHealth() << endl;
+
+    return 0;
+}
